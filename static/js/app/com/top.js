a5134a63ecb1a5eadc271235709e0d9d971186df
hss1994 <1070798770@qq.com>
2017-10-10 09:41:53 +0800

    省市区js调整+判断域名跳转+样式调整

--- static/js/app/com/top.js
+++ static/js/app/com/top.js
@@ -1,6 +1,8 @@
 $('title', window.parent.document).html(OSS.systemName);
 $(function() {
     if (!sessionStorage.getItem('token')) {
+		//判断域名是 hhr开头 合伙人域名 kind为11
+		var kind = document.domain.substr(0, 1)=='h'?'PA':(sessionStorage.getItem('loginKind') || '01')
         location.href = 'signin.html?kind=' + (sessionStorage.getItem('loginKind') || 'P')
         return;
     }
